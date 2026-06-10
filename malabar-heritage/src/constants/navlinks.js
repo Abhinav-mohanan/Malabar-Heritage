ae7a7a5be1d7a8d3c1c8d05e4ea89c4885c49b61
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-06-10 19:02:38 +0530

    feat: implement navbar

---
+++ malabar-heritage/src/constants/navlinks.js
@@ -0,0 +1,9 @@
+
+export const navLinks = [
+  { label: "Home", href: "#" },
+  { label: "About Us", href: "#" },
+  { label: "Products", href: "#" },
+  { label: "Why Choose Us", href: "#" },
+  { label: "Gallery", href: "#" },
+  { label: "Contact", href: "#" },
+];
