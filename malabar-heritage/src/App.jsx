ec35f9c3379888c4b8a28c5fd8c7a953ec6da403
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-06-11 09:17:55 +0530

    feat: implement footer

--- malabar-heritage/src/App.jsx
+++ malabar-heritage/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustBadges from './components/TrustBadges'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <TrustBadges/>
+      <Footer/>
     </div>
     
 
